38a3272a1a43c290788d79a94e4fac92191e8eb0
G-eto <1340878220@qq.com>
2020-09-03 22:16:18 +0800

    all_code

---
+++ src/main/resources/static/js/charts-teacher.js
@@ -0,0 +1,72 @@
+$(function () {
+
+
+    var violet = '#DF99CA',
+        red    = '#F0404C',
+        green  = '#7CF29C';
+
+    // ------------------------------------------------------- //
+    // Charts Gradients
+    // ------------------------------------------------------ //
+    var ctx1 = $("canvas").get(0).getContext("2d");
+    var gradient1 = ctx1.createLinearGradient(150, 0, 150, 300);
+    gradient1.addColorStop(0, 'rgba(210, 114, 181, 0.91)');
+    gradient1.addColorStop(1, 'rgba(177, 62, 162, 0)');
+
+    // ------------------------------------------------------- //
+    // Line Chart
+    // ------------------------------------------------------ //
+    var LINECHARTEXMPLE   = $('#lineChartExample');
+    var lineChartExample = new Chart(LINECHARTEXMPLE, {
+        type: 'line',
+        options: {
+            legend: {labels:{fontColor:"#777", fontSize: 12}},
+            scales: {
+                xAxes: [{
+                    display: true,
+                    gridLines: {
+                        color: '#fff'
+                    }
+                }],
+                yAxes: [{
+                    display: true,
+                    ticks: {
+                        max: 100,
+                        min: 20
+                    },
+                    gridLines: {
+                        color: '#fff'
+                    }
+                }]
+            },
+        },
+        data: {
+            labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+            datasets: [
+                {
+                    label: "Data Set One",
+                    fill: true,
+                    lineTension: 0.3,
+                    backgroundColor: gradient1,
+                    borderColor: 'rgba(210, 114, 181, 0.91)',
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    borderDashOffset: 0.0,
+                    borderJoinStyle: 'miter',
+                    borderWidth: 2,
+                    pointBorderColor: gradient1,
+                    pointBackgroundColor: "#fff",
+                    pointBorderWidth: 2,
+                    pointHoverRadius: 5,
+                    pointHoverBackgroundColor: gradient1,
+                    pointHoverBorderColor: "rgba(220,220,220,1)",
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 1,
+                    pointHitRadius: 10,
+                    data: [10, 50, 33, 71, 49, 55, 35, 40, 30, 50, 25, 40],
+                    spanGaps: false
+                }
+            ]
+        }
+    });
+});
